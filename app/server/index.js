c757517b16d0fe2c7971c2baf27cfdd0ab9f79af
Hurtak <petr.hurtak@gmail.com>
2015-05-17 13:18:03 +0200

    index.js: basic routing and file serving

--- app/server/index.js
+++ app/server/index.js
@@ -2,13 +2,27 @@
 
 var express = require('express');
 var app = express();
+var path = require('path');
+var fs = require('fs');
+
+var basePath = '../'
+var paths = {
+    templ: path.join(__dirname, basePath, 'templ'),
+    js: path.join(__dirname, basePath, 'js'),
+    css: path.join(__dirname, basePath, 'css')
+};
+
+app.use('/js', express.static(paths.js));
+app.use('/css', express.static(paths.css));
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+    res.send('Hello World!');
 });
 
-var server = app.listen(3000, function () {
+app.get('/another', function (req, res) {
+    res.sendFile(path.join(paths.templ, 'index.html'));
+});
 
-  console.log('Example app started.');
-
+var server = app.listen(8000, function () {
+    console.log('Server started.');
 });

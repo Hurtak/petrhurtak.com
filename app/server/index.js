8e43a9158ffe18ab716cd30aa89a4241da53f0d6
Hurtak <petr.hurtak@gmail.com>
2015-05-18 21:45:48 +0200

    index.js: removed useless route, main route leads to index.html

--- app/server/index.js
+++ app/server/index.js
@@ -5,7 +5,7 @@ var app = express();
 var path = require('path');
 var fs = require('fs');
 
-var basePath = '../'
+var basePath = '../';
 var paths = {
     templ: path.join(__dirname, basePath, 'templ'),
     js: path.join(__dirname, basePath, 'js'),
@@ -15,14 +15,10 @@ var paths = {
 app.use('/js', express.static(paths.js));
 app.use('/css', express.static(paths.css));
 
-app.get('/', function (req, res) {
-    res.send('Hello World!');
-});
-
-app.get('/another', function (req, res) {
+app.get('/', function(req, res) {
     res.sendFile(path.join(paths.templ, 'index.html'));
 });
 
-var server = app.listen(8000, function () {
+var server = app.listen(8000, function() {
     console.log('Server started.');
 });

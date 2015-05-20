1376723abb399fa00798235ddeae863c2429361c
Hurtak <petr.hurtak@gmail.com>
2015-05-20 22:02:06 +0200

    started using less

--- app/server/index.js
+++ app/server/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable strict */
 'use strict';
 
 var express = require('express');
@@ -8,16 +9,18 @@ var basePath = '../';
 var paths = {
     templ: path.join(__dirname, basePath, 'templ'),
     js: path.join(__dirname, basePath, 'js'),
-    css: path.join(__dirname, basePath, 'css')
+    css: path.join(__dirname, basePath, 'css'),
+    nodeModules: path.join(__dirname, basePath, '../node_modules')
 };
 
 app.set('view engine', 'ejs');
 
 app.use('/js', express.static(paths.js));
 app.use('/css', express.static(paths.css));
+app.use('/node_modules', express.static(paths.nodeModules));
 
 app.get('/', function(req, res) {
-    res.render(path.join(paths.templ, 'index'), { title: 'The index page!' });
+    res.render(path.join(paths.templ, 'index'), {title: 'The index page!'});
 });
 
 var server = app.listen(8000, function() {

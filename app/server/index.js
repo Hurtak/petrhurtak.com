06cef72f7c69f57c772e06baf6ae43801e034f52
Hurtak <petr.hurtak@gmail.com>
2015-05-26 22:22:54 +0200

    db: started using MySQL

--- app/server/index.js
+++ app/server/index.js
@@ -3,8 +3,18 @@
 
 var express = require('express');
 var app = express();
+
 var path = require('path');
+
 var ejs = require('ejs');
+
+var mysql = require('mysql');
+var db = mysql.createConnection({
+    host: 'localhost',
+    database: 'hurtak_blog',
+    user: 'root',
+    password: ''
+});
 
 var basePath = '../';
 var paths = {
@@ -22,7 +32,19 @@ app.use('/css', express.static(paths.css));
 app.use('/node_modules', express.static(paths.nodeModules));
 
 app.get('/', function(req, res) {
-    res.render(path.join(paths.templ, 'index.html'), {title: 'The index page!'});
+
+    db.query('SELECT content FROM articles LIMIT 1', function(err, rows, fields) {
+        if (err) throw err;
+        var article = rows[0].content;
+
+        var template = {
+            title: 'some title',
+            article: article
+        };
+
+        res.render(path.join(paths.templ, 'index.html'), template);
+    });
+
 });
 
 var server = app.listen(8000, function() {

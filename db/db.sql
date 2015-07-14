4e638939636e77dabf98e9276327f4eb4f182278
Hurtak <petr.hurtak@gmail.com>
2015-07-14 22:59:04 +0200

    database: added article_content table

--- db/db.sql
+++ db/db.sql
@@ -9,15 +9,25 @@ DROP TABLE IF EXISTS `articles`;
 CREATE TABLE `articles` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(50) COLLATE utf8_czech_ci NOT NULL,
-  `preview` varchar(250) COLLATE utf8_czech_ci NOT NULL,
+  `description` varchar(250) COLLATE utf8_czech_ci NOT NULL,
   `url` varchar(50) COLLATE utf8_czech_ci NOT NULL,
-  `directory` varchar(100) COLLATE utf8_czech_ci NOT NULL,
   `publication_date` datetime NOT NULL,
   `last_update` datetime NOT NULL,
   `visible` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
+  UNIQUE KEY `url` (`url`),
   KEY `publication_date` (`publication_date`),
   KEY `visible` (`visible`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
+
+
+DROP TABLE IF EXISTS `article_content`;
+CREATE TABLE `article_content` (
+  `id` int(11) NOT NULL,
+  `article_id` int(11) NOT NULL,
+  `content` varchar(10000) COLLATE utf8_czech_ci NOT NULL,
+  KEY `article_id` (`article_id`),
+  CONSTRAINT `article_content_ibfk_2` FOREIGN KEY (`article_id`) REFERENCES `articles` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
 
 
@@ -32,4 +42,4 @@ CREATE TABLE `fulltext` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
 
 
--- 2015-06-20 11:54:35
+-- 2015-07-14 20:57:47

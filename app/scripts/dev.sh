b5e983f810e21fcb4f5f61e83d8b6593abe2938f
Petr Huřťák <petr.hurtak@gmail.com>
2016-06-01 20:06:21 +0200

    dev.sh: use local copy of codemon & add proper file watching

--- app/scripts/dev.sh
+++ app/scripts/dev.sh
@@ -1,2 +1,19 @@
+### Directory setup 
+cd "$(dirname "$0")" # cd into directory where script is located
+cd .. # cd into app director
+
+### EXPORTED VARIABLES
 export NODE_ENV="development"
-nodemon
+
+### LOCAL VARIABLES
+NODE_BIN="node_modules/.bin" 
+
+### START SERVER
+${NODE_BIN}/nodemon src/server/index.js \
+  --ext js,html,json # extensions to look for when file wathing
+  --watch src/templates \
+  --watch src/server \
+  --watch src/config
+   
+
+

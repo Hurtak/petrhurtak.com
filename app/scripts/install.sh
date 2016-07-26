8680b7f93738d9ad5de7443c376aec512d5bc0f6
Petr Huřťák <petr.hurtak@gmail.com>
2016-07-26 11:30:23 +0200

    server: cleanup install script

--- app/scripts/install.sh
+++ app/scripts/install.sh
@@ -5,7 +5,6 @@ cd "$(dirname "$0")" # cd into directory where script is located
 cd .. # cd into app director
 
 ### PREPARE DIRECTORIES FOR APP
-mkdir -p www
 mkdir -p www/log
 mkdir -p www/articles
 

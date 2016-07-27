2ce33dc12496427aaaafb6980ced7d989b64a43e
Petr Huřťák <petr.hurtak@gmail.com>
2016-07-27 23:26:52 +0200

    articles: remove old functions which are not needed after refactoring

--- app/src/server/articles.js
+++ app/src/server/articles.js
@@ -27,38 +27,6 @@ function findPathToArticle (directory, articleName, searchedDepth, currentDepth 
   return false
 }
 
-function getArticlesMetadata (directory, filename, gatheredMetadata = [], baseDirectory = directory) {
-  const list = fs.readdirSync(directory) // TODO: sync function
-  for (const file of list) {
-    const filePath = path.join(directory, file)
-    const isDirectory = fs.statSync(filePath).isDirectory() // TODO: sync function
-
-    if (isDirectory) {
-      gatheredMetadata = getArticlesMetadata(filePath, filename, gatheredMetadata, baseDirectory)
-    } else if (file === filename) {
-      // path to article relative to baseDirectory
-      const articleDirectory = filePath
-        .replace(filename, '')
-        .replace(baseDirectory, '')
-
-      const metadata = frontMatter(fs.readFileSync(filePath, 'utf8')).attributes // TODO: sync function
-
-      metadata.directory = articleDirectory
-      metadata.publication_date = utilsArticles.isoStringToUtcDate(metadata.publication_date)
-      metadata.last_update = utilsArticles.isoStringToUtcDate(metadata.last_update)
-
-      const articlePath = articleDirectory
-        .split(path.sep)
-        .filter(x => x)
-
-      metadata.url = articlePath[articlePath.length - 1]
-      gatheredMetadata.push(metadata)
-    }
-  }
-
-  return gatheredMetadata
-}
-
 function getArticlesDirectories (directory, searchedDepth, currentDepth = 0, articlesList = []) {
   const list = fs.readdirSync(directory) // TODO: sync function
 
@@ -76,64 +44,6 @@ function getArticlesDirectories (directory, searchedDepth, currentDepth = 0, art
   }
 
   return articlesList
-}
-
-function parseArticle (articlePath) {
-  let data = fs.readFileSync(articlePath, 'utf8')
-  data = frontMatter(data)
-
-  const metadata = data.attributes
-
-  const articleDirectory = '/static/articles/' +
-    articlePath
-      // c:\some\path\rootdir\article.md -> rootdir\article.md
-      .replace(paths.articles, '')
-      .split(path.sep)
-      // remove empty values (\some\path creates ['', 'some', 'path'])
-      .filter(value => value)
-      // remove filename rootdir\article.md - > rootdir
-      .filter((value, index, array) => index !== array.length - 1)
-      // use '/' instead of path.sep, because that's what we are using in templates
-      .join('/')
-
-  // TODO: every time we make html transformation we take html
-  //       string and pass it into cheerio and create cheerio
-  //       object, then make transformations and then transform
-  //       back to html string. We could pass around cheerio
-  //       object so creation of cheerio object and transformation
-  //       to html string will be done only once
-  let article = data.body
-
-  // inside articles we are using <xmp> instead of <code>, so transform it to <code> before we
-  // run other transformations which might depend on <code> tag being used instead of <xmp>
-  article = utilsArticles.changeXmpToCode(article)
-  article = utilsArticles.trimCodeBlocks(article)
-  article = utilsArticles.removeIndentationInCodeBlocks(article)
-  article = utilsArticles.escapeAndHighlightCodeBlocks(article)
-
-  article = utilsArticles.addIdsToHeadings(article)
-
-  article = utilsArticles.relativeUrlToAbsolute(article, 'img', 'src', articleDirectory)
-
-  // TODO: think about merging these two together, or at least share css selector?
-  article = utilsArticles.enhanceSnippetLinks(article)
-  article = utilsArticles.relativeUrlToAbsolute(article, 'a[href^="./snippets/"]', 'href', articleDirectory)
-
-  article = htmlMinifier.minify(article, {
-    collapseWhitespace: true,
-    conservativeCollapse: true,
-    minifyCSS: true,
-    minifyJS: true,
-    removeComments: true,
-    removeRedundantAttributes: true,
-    sortAttributes: true,
-    sortClassName: true
-  })
-
-  return {
-    metadata,
-    html: article
-  }
 }
 
 function getArticle (articlePath) {
@@ -233,8 +143,6 @@ function getArticleData (pathToArticle) {
 
 module.exports = {
   findPathToArticle,
-  getArticlesMetadata,
   getArticlesDirectories,
-  parseArticle,
   getArticleData
 }

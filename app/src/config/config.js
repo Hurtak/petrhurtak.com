6e51f9c4725df89641321b9268625e6de9becc2c
Petr Huřťák <petr.hurtak@gmail.com>
2016-09-25 10:32:23 +0200

    config.js: rename app specific env variables

--- app/src/config/config.js
+++ app/src/config/config.js
@@ -2,14 +2,14 @@
 
 module.exports = {
   database: {
-    host: process.env.NODE_DB_HOST || 'localhost',
-    database: process.env.NODE_DB_NAME || 'hurtak_blog',
-    user: process.env.NODE_DB_USER || 'root',
-    password: process.env.NODE_DB_PASS || '',
+    host: process.env.APP_DB_HOST || 'localhost',
+    database: process.env.APP_DB_NAME || 'hurtak_blog',
+    user: process.env.APP_DB_USER || 'root',
+    password: process.env.APP_DB_PASS || '',
     multipleStatements: true
   },
 
-  port: process.env.NODE_PORT || 8000,
+  port: process.env.APP_PORT || 8000,
 
   devel: process.env.NODE_ENV !== 'production',
   production: process.env.NODE_ENV === 'production'

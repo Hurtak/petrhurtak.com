03189aca593e6f80f703c83adde277b09b768657
Petr Huřťák <petr.hurtak@firma.seznam.cz>
2016-11-17 18:35:21 +0100

    docker: finally make connection between app & db containers work

--- app/src/config/config.js
+++ app/src/config/config.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   database: {
-    host: process.env.APP_DB_HOST || 'localhost',
+    host: process.env.APP_DB_HOST || 'blog-db',
     database: process.env.APP_DB_NAME || 'hurtak_blog',
     user: process.env.APP_DB_USER || 'root',
     password: process.env.APP_DB_PASS || 'root',

ef1a32f0ae8f124a68fc38f5d0c81457576750a2
Petr Huřťák <petr.hurtak@firma.seznam.cz>
2016-11-27 13:50:01 +0100

    paths.js: make paths relative to the root directory

--- src/server/paths.js
+++ src/server/paths.js
@@ -2,36 +2,35 @@
 
 const path = require('path')
 
-function absolutePath (relativePath) {
-  return path.join(__dirname, '../', relativePath)
-}
+const absolutePath = relativePath => path.join(__dirname, '../../', relativePath)
 
-// paths relative to src directory
+// paths relative to the repository root directory
 const paths = {
-  server: absolutePath('./server'),
-  templates: absolutePath('./templates'),
+  server: absolutePath('./src/server'),
 
-  static: absolutePath('./static'),
-  styles: absolutePath('./static/styles'),
-  scripts: absolutePath('./static/scripts'),
-  images: absolutePath('./static/images'),
+  templates: absolutePath('./src/templates'),
 
-  dist: absolutePath('../dist'),
-  distDrafts: absolutePath('../dist/_drafts'),
-  distStatic: absolutePath('../dist/static'),
-  distStyles: absolutePath('../dist/static/styles'),
-  distScripts: absolutePath('../dist/static/scripts'),
-  distImages: absolutePath('../dist/static/images'),
-  distNodeModules: absolutePath('../dist/static/node_modules'),
+  static: absolutePath('./src/static'),
+  styles: absolutePath('./src/static/styles'),
+  scripts: absolutePath('./src/static/scripts'),
+  images: absolutePath('./src/static/images'),
 
-  articles: absolutePath('../articles'),
-  articlesDrafts: absolutePath('../articles/_drafts'),
+  dist: absolutePath('./dist'),
+  distDrafts: absolutePath('./dist/_drafts'),
+  distStatic: absolutePath('./dist/static'),
+  distStyles: absolutePath('./dist/static/styles'),
+  distScripts: absolutePath('./dist/static/scripts'),
+  distImages: absolutePath('./dist/static/images'),
+  distNodeModules: absolutePath('./dist/static/node_modules'),
+
+  articles: absolutePath('./articles'),
+  articlesDrafts: absolutePath('./articles/_drafts'),
   articleMarkdown: 'article.md',
   articleMetadata: 'metadata.yaml',
   articleImages: 'images',
   articleSnippets: 'snippets',
 
-  nodeModules: absolutePath('../node_modules')
+  nodeModules: absolutePath('./node_modules')
 }
 
 module.exports = paths

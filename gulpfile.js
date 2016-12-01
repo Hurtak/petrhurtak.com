1311dbdc7525fa4b8c0e76c4f2894b013442a50e
Petr Huřťák <petr.hurtak@firma.seznam.cz>
2016-12-01 21:48:57 +0100

    build: move index.js build stuff into gulpfile.js tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,148 @@
-const gulp = require('gulp')
-const execa = require('execa')
+'use strict'
 
-gulp.task('compile', function () {
-  return execa('node', ['src/server/index.js']).then(result => {
-    console.log(result.stdout)
-  })
+const gulp = require('gulp')
+const path = require('path')
+const fs = require('fs-promise')
+const lodash = require('lodash')
+// const htmlMinifier = require('html-minifier')
+
+const debug = require('./src/server/debug.js')
+const paths = require('./src/server/paths.js')
+const config = require('./src/server/config.js')
+const articles = require('./src/server/articles.js')
+const nunjucks = require('./src/server/nunjucks/env.js')
+
+debug()
+
+gulp.task('prepare-dirs', function (done) {
+  fs.remove(paths.dist)
+    .then(() => {
+      return fs.mkdir(paths.dist)
+    })
+    .then(() => {
+      return Promise.all([
+        fs.mkdir(paths.distDrafts),
+        fs.mkdir(paths.distStatic)
+      ])
+    })
+    .then(() => done())
 })
 
-gulp.task('default', gulp.series('compile'))
+gulp.task('404', function (done) {
+  const html404 = nunjucks.render('404.njk')
 
-// gulp.watch('')
+  fs.writeFile(path.join(paths.dist, '404.html'), html404)
+    .then(done)
+})
+
+gulp.task('robots.txt', function (done) {
+  const htmlRobotsTxt = nunjucks.render('robots.txt.njk')
+
+  fs.writeFile(path.join(paths.dist, 'robots.txt'), htmlRobotsTxt)
+    .then(done)
+})
+
+gulp.task('static', function (done) {
+  Promise.all([
+    fs.symlink(paths.styles, paths.distStyles),
+    fs.symlink(paths.scripts, paths.distScripts),
+    fs.symlink(paths.images, paths.distImages),
+    fs.symlink(paths.nodeModules, paths.distNodeModules)
+  ]).then(() => done())
+})
+
+gulp.task('articles', function (done) {
+  // gather articles data
+
+  let articlesData = articles.getArticles(paths.articles, paths.articlesDrafts)
+  articlesData = lodash.sortBy(articlesData, 'metadata.dateLastUpdate')
+  articlesData = articlesData.reverse()
+
+  let articlesPublishedData = lodash.filter(articlesData, article => article.metadata.published === true)
+  articlesPublishedData = lodash.filter(articlesPublishedData, article => article.metadata.dateLastUpdate <= new Date())
+
+  // index page
+  const indexArticles = lodash.slice(articlesPublishedData, 0, config.articles.perPage)
+  const htmlIndex = nunjucks.render('index.njk', {articles: indexArticles})
+  fs.writeFileSync(path.join(paths.dist, 'index.html'), htmlIndex)
+
+  // articles
+  const articlePromises = []
+  for (const article of articlesData) {
+    // article directory
+    const folder = path.join(
+      article.metadata.published ? paths.dist : paths.distDrafts,
+      article.metadata.url
+    )
+
+    const promise = fs.mkdir(folder)
+      .then(() => {
+        // article html
+        const htmlArticle = nunjucks.render('article.njk', article)
+
+        const indexHtml = fs.writeFile(path.join(folder, 'index.html'), htmlArticle)
+        const images = fs.symlink(
+          path.join(article.fs.path, paths.articleImages),
+          path.join(folder, paths.articleImages)
+        )
+        const snippets = fs.symlink(
+          path.join(article.fs.path, paths.articleSnippets),
+          path.join(folder, paths.articleSnippets)
+        )
+
+        return Promise.all([
+          indexHtml,
+          images,
+          snippets
+        ])
+      })
+
+    articlePromises.push(promise)
+  }
+
+  const rssPromise = Promise.resolve()
+    .then(() => {
+      const rssArticles = lodash.slice(articlesData, 0, config.articles.perRssFeed)
+
+      const rssFeed = nunjucks.render('rss.njk', {articles: rssArticles})
+
+      return fs.writeFile(path.join(paths.dist, 'rss.xml'), rssFeed)
+    })
+
+  const humansTxtPromise = Promise.resolve()
+    .then(() => {
+      const lastUpdate = articlesPublishedData[0].metadata.dateLastUpdate
+      const humansTxt = nunjucks.render('humans.txt.njk', {lastUpdate: lastUpdate})
+
+      return fs.writeFileSync(path.join(paths.dist, 'humans.txt'), humansTxt)
+    })
+
+  Promise
+    .all([
+      articlePromises,
+      rssPromise,
+      humansTxtPromise
+    ])
+    .then(() => done())
+})
+
+gulp.task('default', gulp.series(
+  'prepare-dirs',
+  gulp.parallel(
+    '404',
+    'robots.txt',
+    'static',
+    'articles'
+  )
+))
+
+// articleHtml = htmlMinifier.minify(articleHtml, {
+//   collapseWhitespace: true,
+//   conservativeCollapse: true,
+//   minifyCSS: true,
+//   minifyJS: true,
+//   removeComments: true,
+//   removeRedundantAttributes: true,
+//   sortAttributes: true,
+//   sortClassName: true
+// })

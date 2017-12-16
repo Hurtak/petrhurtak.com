32a03fb2028b99f7120b0e54c4c0d0c68050f167
Petr Huřťák <petr.hurtak@firma.seznam.cz>
2017-12-16 21:15:14 +0100

    rewrite: head tag

--- nextjs/pages/_document.js
+++ nextjs/pages/_document.js
@@ -1,3 +1,4 @@
+import React from "react";
 import Document, { Head, Main, NextScript } from "next/document";
 import { renderStatic } from "glamor/server";
 
@@ -18,10 +19,28 @@ export default class MyDocument extends Document {
 
   render() {
     return (
-      <html>
+      <html lang="en">
         <Head>
-          <title>With Glamorous</title>
+          {/* TODO: check if it will get duplicated by Next */}
+          <meta charSet="utf-8" />
+          <title>Hurtak.cc</title>
+
+          {/* Favicons */}
+          <link
+            rel="icon"
+            type="image/png"
+            href="/static/favicons/favicon.png"
+            sizes="16x16"
+          />
+
+          {/* Meta */}
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+
           <style dangerouslySetInnerHTML={{ __html: this.props.css }} />
+
+          {/* Extra */}
+          <link rel="alternate" type="application/rss+xml" href="/rss.xml" />
+          <link type="text/plain" rel="author" href="/humans.txt" />
         </Head>
         <body>
           <Main />

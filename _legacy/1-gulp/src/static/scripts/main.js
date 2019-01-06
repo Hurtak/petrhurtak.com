dd45294c101311adfdc2598732a0ff4e1f19adbc
Petr Huřťák <petr.hurtak@gmail.com>
2019-01-06 23:14:24 +0100

    cra: remove unused legacy stuff

--- _legacy/1-gulp/src/static/scripts/main.js
+++
@@ -1,9 +0,0 @@
-(function main() {
-  "use strict";
-
-  const data = window.appData;
-
-  window.App.DateConvertor.init();
-  window.App.Snippets.init(data.snippets);
-  window.App.Log.init();
-})();

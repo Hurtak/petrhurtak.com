365adbd3a8cfcb8831621098a5f4b158374f7019
Petr Huřťák <petr.hurtak@gmail.com>
2019-05-13 17:37:03 +0200

    switch-node-version: publish

---
+++ blog/src/articles/published/2019-05-13--switch-node-version/article.jsx
@@ -0,0 +1,74 @@
+import React from "react";
+import { P, H1, List, Li, Link, Italic, Table, Tr, Tc, Code } from "../../components";
+
+export default () => (
+  <>
+    <P>
+      The easiest way to install and switch between different versions of <Italic>Node.js</Italic> is with{" "}
+      <Link href="https://brew.sh/">Homebrew</Link> package manager.
+    </P>
+
+    <Table
+      heading={
+        <Tr>
+          <Tc>Action</Tc>
+          <Tc>Command</Tc>
+        </Tr>
+      }
+    >
+      <Tr>
+        <Tc>Current Node version</Tc>
+        <Tc>
+          <Code language="bash">node --version</Code>
+        </Tc>
+      </Tr>
+      <Tr>
+        <Tc>Search available and installed versions</Tc>
+        <Tc>
+          <Code language="bash">brew search node</Code>
+        </Tc>
+      </Tr>
+      <Tr>
+        <Tc>Install latest version</Tc>
+        <Tc>
+          <Code language="bash">brew install node</Code>
+        </Tc>
+      </Tr>
+      <Tr>
+        <Tc>Install specific version</Tc>
+        <Tc>
+          <Code language="bash">brew install node@8</Code>
+        </Tc>
+      </Tr>
+      <Tr>
+        <Tc>Unlink current node version</Tc>
+        <Tc>
+          <Code language="bash">brew unlink node</Code>
+        </Tc>
+      </Tr>
+      <Tr>
+        <Tc>Link older version</Tc>
+        <Tc>
+          <Code language="bash">brew link node@8</Code>
+        </Tc>
+      </Tr>
+      <Tr>
+        <Tc>Link latest version</Tc>
+        <Tc>
+          <Code language="bash">brew link node</Code>
+        </Tc>
+      </Tr>
+    </Table>
+
+    <H1>Alternatives</H1>
+
+    <List>
+      <Li>
+        <Link>https://github.com/nvm-sh/nvm</Link>
+      </Li>
+      <Li>
+        <Link>https://github.com/tj/n</Link>
+      </Li>
+    </List>
+  </>
+);
---
+++ blog/src/articles/published/2019-05-13--switch-node-version/metadata.ts
@@ -0,0 +1,15 @@
+import { IArticleMetadataRaw } from "../../articles";
+
+const metadata: IArticleMetadataRaw = {
+  title: "How to switch Node version on macOS",
+
+  description: `
+    How to install Node.js on MacOS and switch between different versions.
+  `,
+
+  slug: "switch-node-version",
+
+  datePublication: "2019-05-13 15:00:00",
+  dateLastUpdate: "2019-05-13 15:00:00"
+};
+export default metadata;

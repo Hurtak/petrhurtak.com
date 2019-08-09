c43bab4f5912c98c417d8dbaf8a829dea75bbef2
Petr Huřťák <petr.hurtak@gmail.com>
2019-08-09 10:31:03 +0200

    squash-commits: add missing space

--- blog/src/articles/published/2019-08-08--squash-commits/article.jsx
+++ blog/src/articles/published/2019-08-08--squash-commits/article.jsx
@@ -7,10 +7,10 @@ export default () => (
 
     <P>
       If you search Google about how to squash all commits, you will often find confusing tutorials that will either
-      recommend using <Code language="bash">git rebase</Code>
-      (where you have to manually specify which commits you want to squash), or tutorials recommending using{" "}
-      <Code language="bash">git reset</Code> or <Code language="bash">git rebase</Code> on latest <Code>n</Code> commits
-      (where you need to know how many commits you made in your branch). But there is an easier way.
+      recommend using <Code language="bash">git rebase</Code> (where you have to manually specify which commits you want
+      to squash), or tutorials recommending using <Code language="bash">git reset</Code> or{" "}
+      <Code language="bash">git rebase</Code> on latest <Code>n</Code> commits (where you need to know how many commits
+      you made in your branch). But there is an easier way.
     </P>
 
     <H1>One liner solution</H1>

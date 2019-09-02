f72e86435f64222247800b60e06c7b17d3f40828
Petr Huřťák <petr.hurtak@gmail.com>
2019-09-02 23:01:27 +0200

    blog: add typescript syntax highlight

--- blog/src/app/components/article/code.tsx
+++ blog/src/app/components/article/code.tsx
@@ -17,6 +17,7 @@ import { highlightCss } from "../../../generated/raw-files";
 import highlight from "highlight.js/lib/highlight.js";
 
 import highlightLanguageJavaScript from "highlight.js/lib/languages/javascript";
+import highlightLanguageTypeScript from "highlight.js/lib/languages/typescript";
 import highlightLanguageMakefile from "highlight.js/lib/languages/makefile";
 import highlightLanguageJson from "highlight.js/lib/languages/json";
 import highlightLanguageYaml from "highlight.js/lib/languages/yaml";
@@ -32,6 +33,7 @@ import highlightLanguageElm from "highlight.js/lib/languages/elm";
 // import highlightMarkdown from "highlight.js/lib/languages/markdown";
 
 highlight.registerLanguage("javascript", highlightLanguageJavaScript);
+highlight.registerLanguage("typescript", highlightLanguageTypeScript);
 highlight.registerLanguage("makefile", highlightLanguageMakefile);
 highlight.registerLanguage("json", highlightLanguageJson);
 highlight.registerLanguage("yaml", highlightLanguageYaml);
--- blog/src/app/typings/highlight.d.ts
+++ blog/src/app/typings/highlight.d.ts
@@ -1,11 +1,15 @@
 // TODO: this should not be needed because we hawe typings??
 
 declare module "highlight.js/lib/highlight.js" {
-  export function registerLanguage(lang: string, highligher: any): any;
+  export function registerLanguage(lang: string, module: any): any;
   export function highlight(lang: string, code: string): { value: string };
 }
 
 declare module "highlight.js/lib/languages/javascript" {
+  const x: any;
+  export default x;
+}
+declare module "highlight.js/lib/languages/typescript" {
   const x: any;
   export default x;
 }

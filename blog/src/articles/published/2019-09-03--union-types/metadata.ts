e8f11e723407080bcff4cb8bb716ddc8c94458f1
Petr Huřťák <petr.hurtak@gmail.com>
2019-09-03 17:03:13 +0200

    union-type:  release

---
+++ blog/src/articles/published/2019-09-03--union-types/metadata.ts
@@ -0,0 +1,17 @@
+import { IArticleMetadataRaw } from "../../articles";
+
+const metadata: IArticleMetadataRaw = {
+  title: "Union types",
+
+  description: `
+    One of the most useful features of typed languages are union types. They
+    bring interesting patterns on how to model state transitions that could
+    be used even in dynamic languages.
+  `,
+
+  slug: "union-types",
+
+  datePublication: "2019-09-03 15:00:00",
+  dateLastUpdate: "2019-09-03 15:00:00"
+};
+export default metadata;

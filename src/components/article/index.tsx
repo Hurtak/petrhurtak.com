81fa23917b53352a771e4771864245c4c6b4f334
Petr Hurtak <petr.hurtak@gmail.com>
2022-02-05 14:32:36 +0100

    add more article components

--- src/components/article/index.tsx
+++ src/components/article/index.tsx
@@ -1,9 +1,11 @@
 export { Dash } from "../base/dash";
+export { Hellip } from "../base/hellip";
 export { Link } from "../base/link";
 export { Br } from "./br";
 export { Code } from "./code";
 export { Image } from "./image";
 export { Li, List } from "./list";
+export { Quote } from "./quote";
 export { Table, Tc, Tr } from "./table";
 export { H1, H2, Italic, P, Strong } from "./text";
 export { Video } from "./video";
--- src/components/article/list.tsx
+++ src/components/article/list.tsx
@@ -1,5 +1,6 @@
-export const List = ({ children }: { children: React.ReactNode }) => {
-  return <ul>{children}</ul>;
+export const List = ({ numbered = false, children }: { numbered?: boolean; children: React.ReactNode }) => {
+  const Tag = numbered ? "ol" : "ul";
+  return <Tag>{children}</Tag>;
 };
 
 export const Li = ({ children }: { children: React.ReactNode }) => {

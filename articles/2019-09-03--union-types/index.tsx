8eca5e8ad87a68ec49ee047df46db7a720266ea7
Petr Hurtak <petr.hurtak@gmail.com>
2022-05-27 20:47:08 +0200

    union-types: cleanup

--- articles/2019-09-03--union-types/index.tsx
+++ articles/2019-09-03--union-types/index.tsx
@@ -4,27 +4,23 @@ export const Article = () => (
   <>
     <P>
       Discriminate union types (also called <Italic>tagged unions</Italic> or <Italic>algebraic data types</Italic>) are
-      very helpful and are in my personal top 2 of must-have advanced features of any typed language, along with{" "}
-      <Link href="https://www.typescriptlang.org/docs/handbook/advanced-types.html#nullable-types">
-        non-nullable types
-      </Link>
-      .
+      one of my favorite features of any typed language.
     </P>
 
     <P>
       They can be used to cleanly model and describe state transitions in your business logic and your UI. They are
-      worth knowing about because patterns emerging from these can also be used in dynamic languages without the need of
-      type system.
+      worth knowing about because patterns emerging from these can also be used in dynamic languages without the need
+      for a type system.
     </P>
 
     <H1>HTTP request example</H1>
 
     <P>
-      Let's take a simple example of making API HTTP request and displaying the response in the UI. Since the request
-      takes some time to finish, or it can fail, we will also need to handle these cases.
+      Let's take a simple example of making an HTTP request and displaying the response in the UI. With these async
+      operations you also need to handle the loading and error case and display it to the user.
     </P>
 
-    <H1>"Classic" approach</H1>
+    <H1>"Traditional" approach</H1>
 
     <Code language="jsx">{`
       // Initial state
@@ -38,17 +34,16 @@ export const Article = () => (
       };
 
       try {
-        const request = await fetch(apiUrl);
-        const requestData = await request.text();
+        const requestData = await getApiData();
 
-        // Data is downloaded
+        // Data is fetched
         apiData = {
           error: false,
           loading: false,
           data: requestData
         };
       } catch (error) {
-        // Error state
+        // Error with the request
         apiData = {
           error: error,
           loading: false,
@@ -59,11 +54,9 @@ export const Article = () => (
       // Somewhere in your UI code
       if (!apiData) {
         return <p>Initial state</p>;
-      }
-      if (apiData.loading) {
+      } else if (apiData.loading) {
         return <p>Loading</p>;
-      }
-      if (apiData.error) {
+      } else if (apiData.error) {
         return <p>Error</p>;
       }
 
@@ -80,12 +73,12 @@ export const Article = () => (
 
     <List>
       <Li>
-        Instead of clear indication of which type of state we are in, there are a bunch of boolean flags or
+        Instead of a clear indication of which type of state we are in, there are a bunch of boolean flags or
         empty/non-empty data values and we need to decide which has higher priority and what combination of these fields
-        results in corresponding state type.
+        results in the corresponding state type.
       </Li>
       <Li>
-        Data structure can end up in states that do not make sense, like <Code>loading: true</Code> and{" "}
+        The data structure can end up in states that do not make sense, like <Code>loading: true</Code> and{" "}
         <Code>error: true</Code> at the same time.
       </Li>
       <Li>
@@ -113,16 +106,15 @@ export const Article = () => (
       };
 
       try {
-        const request = await fetch(apiUrl);
-        const requestData = await request.text();
+        const requestData = await getApiData();
 
-        // Data is downloaded
+        // Data is fetched
         apiData = {
           type: "FINISHED",
           data: requestData
         };
       } catch (error) {
-        // Error state
+        // Error with the request
         apiData = {
           type: "ERROR",
           error: error
@@ -145,16 +137,16 @@ export const Article = () => (
     `}</Code>
 
     <P>
-      This is much better now because we have clear indicator what state transition we are in, and we do not need to
-      check boolean flags that can be mutually exclusive. Also the current state we are in only has data relevant to it,
-      so for example, when we are in loading state there is no empty data field and false in error field.
+      This is much better now because we have a clear indicator of what state transition we are in, and we do not need
+      to check boolean flags that can be mutually exclusive. Also the current state we are in only has data relevant to
+      it, so for example, when we are in the loading state there is no empty data field and false in the error field.
     </P>
 
     <H1>TypeScript</H1>
 
     <P>
       If you have typed language that supports union types you can take advantage of the type system to enforce these
-      rules. Here is how it would look like in TypeScript
+      rules during compile time. Here is how it would look like in TypeScript.
     </P>
 
     <Code language="ts">{`
@@ -195,49 +187,7 @@ export const Article = () => (
           return <p>Api data: {apiData.data}</p>;
         default:
           throw new Error(\`Unknown apiData.type "\${apiData.type}"\`);
-        }
-    `}</Code>
-
-    <P>
-      What we can then do is use the{" "}
-      <Link href="https://basarat.gitbooks.io/typescript/docs/types/never.html">never type</Link>, to opt into
-      exhaustiveness checking. Meaning you will need to handle all cases of your union type, otherwise, the compiler
-      will complain. This is another very useful compiler check that you will appreciate as your codebase grows, and in
-      case you ever need to extend your union type, like adding <Code>CANCELLED</Code> state to your HTTP request type,
-      it will force you to update all pieces of code where given union type is used.
-    </P>
-
-    <Code language="tsx">{`
-      type HttpData<T> =
-        | { type: "INITIAL" }
-        | { type: "LOADING" }
-        | { type: "ERROR"; error: any }
-        | { type: "FINISHED"; data: T };
-
-      const apiData: HttpData<string> = {
-        type: "FINISHED",
-        data: "api response data"
-      };
-
-      function never(value: never): never {
-        throw new Error("This part of code should never be reached. " + JSON.stringify(value));
       }
-
-      // Somewhere in your UI code
-      switch (apiData.type) {
-        case "INITIAL":
-          return <p>Initial state</p>;
-        case "LOADING":
-          return <p>Loading</p>;
-        case "ERROR":
-          return <p>Error</p>;
-        // case "FINISHED":
-        //   return <p>Api data: {apiData.data}</p>;
-        default:
-          // If you do not handle all cases, TS will complain:
-          //   Argument of type '{ type: "FINISHED"; data: string; }' is not assignable to parameter of type 'never'.
-          return never(apiData);
-        }
     `}</Code>
   </>
 );

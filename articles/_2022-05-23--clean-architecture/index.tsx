b5b65473bc43cd6c9b0c39997b3468be3632611e
Petr Hurtak <petr.hurtak@gmail.com>
2022-05-28 13:52:54 +0200

    clean architecture draft

---
+++ articles/_2022-05-23--clean-architecture/index.tsx
@@ -0,0 +1,9 @@
+import { Code, H1 } from "../../src/components/article";
+
+export const Article = () => (
+  <>
+    <H1>Hello</H1>
+
+    <Code>https://www.example.com/products/computers.php?type=laptop#return-policy</Code>
+  </>
+);

fcad086ecdb45115ae4679b199c0c40d0f50c7b7
Petr Hurtak <petr.hurtak@gmail.com>
2022-05-28 13:47:19 +0200

    disable GA in dev

--- src/services/google-analytics.ts
+++ src/services/google-analytics.ts
@@ -1,12 +1,18 @@
 import { useCallback, useEffect } from "react";
 import reactGA from "react-ga4";
 
+import { config } from "../config";
+
 export const useGoogleAnalytics = ({ token }: { token: string }): { setCurrentPage: (path: string) => void } => {
   useEffect(() => {
+    if (config.isDev) return;
+
     reactGA.initialize(token);
   }, [token]);
 
   const setCurrentPage = useCallback((path: string) => {
+    if (config.isDev) return;
+
     reactGA.send({ hitType: "pageview", page: path });
   }, []);
 
